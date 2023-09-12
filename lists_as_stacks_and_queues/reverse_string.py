3e3b41bca1c525e32404755ade6e18d4f14e7bf6
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-12 19:28:37 +0300

    Update reverse_string.py

--- lists_as_stacks_and_queues/reverse_string.py
+++ lists_as_stacks_and_queues/reverse_string.py
@@ -3,3 +3,13 @@ print(word)
 
 while word:
     print(word.pop(), end='')
+
+# -----------------------------------------------------------------------------------------
+
+text = list(input())
+stack = []
+
+for i in range(len(text)):
+    stack.append(text.pop())
+
+print("".join(stack))
