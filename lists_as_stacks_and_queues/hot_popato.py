749f6129a93c86bd7a64c73bf0f7aa5456fa2d39
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-12 21:19:01 +0300

    Update hot_popato.py

--- lists_as_stacks_and_queues/hot_popato.py
+++ lists_as_stacks_and_queues/hot_popato.py
@@ -4,7 +4,21 @@ children = deque(input().split())
 n = int(input()) - 1
 
 while len(children) != 1:
-    children.rotate(-n)
+    children.rotate(-n) # removes the first on the queue and adds it to the last position
+    print(f"Removed {children.popleft()}")
+
+print(f"Last is {children.popleft()}")
+
+# OR ---------------------------------------------
+
+children = deque(input().split())
+n = int(input()) - 1
+
+while len(children) != 1:
+    count = 0
+    while count != n:
+        removed = children.popleft()
+        children.append(removed)
     print(f"Removed {children.popleft()}")
 
 print(f"Last is {children.popleft()}")
