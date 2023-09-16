5515ebbd752a2b65a7c84e91abccb1326f686798
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-16 21:12:59 +0300

    Update students_grades.py

--- tuples_and_sets/students_grades.py
+++ tuples_and_sets/students_grades.py
@@ -2,13 +2,30 @@ number_of_students = int(input())
 
 students = {}
 
+for _ in range(number_of_students):
+    name, grade = input().split()
+
+    if name not in students:
+        students[name] = []
+    students[name].append(float(grade))
+
+for student_name, grades in students.items():
+    formatted_grades = ' '.join([f"{grade:.2f}" for grade in grades])
+    print(f"{student_name} -> {formatted_grades} (avg: {(sum(grades) / len(grades)):.2f}")
+
+
+# OR ----------------------------------------------------------------
+
+number_of_students = int(input())
+
+students = {}
+
 for student in range(number_of_students):
     student_name, grade = input().split()
-    grade = float(grade)
 
     if student_name not in students:
         students[student_name] = []
-    students[student_name].append(grade)
+    students[student_name].append(float(grade))
 
 for key, value in students.items():
     average_grade = sum(value) / len(value)
