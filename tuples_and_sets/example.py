11c31ba092e731a067d67fffaf83e151b9c604cf
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-16 14:03:57 +0300

    add

---
+++ tuples_and_sets/example.py
@@ -0,0 +1,15 @@
+# TUPLE
+
+# count
+numbers = (1, 2, 1, 3, 1)
+numbers.count(1) # returns the number of times a specific value occurs / 3 times '1'
+
+# index
+names = ("Pesho", "Gosho", "Gosho")
+names.index("Gosho") # returns the index of a particular element // index = 1 in this case
+
+# unpacking
+nums = (1, 2, 3)
+a, b, c = nums # returns a = 1, b = 2, c = 3 /// if we use less or more variables to unpack it will return an error
+
+
