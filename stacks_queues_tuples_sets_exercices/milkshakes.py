d2ccf701278778ac166dd14f73617e135663541e
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-19 20:00:28 +0300

    Update milkshakes.py

--- stacks_queues_tuples_sets_exercices/milkshakes.py
+++ stacks_queues_tuples_sets_exercices/milkshakes.py
@@ -1,5 +1,40 @@
 from collections import deque
 
+chocolates_available = [int(x) for x in input().split(', ')]
+milk_cups = deque(int(x) for x in input().split(', '))
+milkshakes_prepared = 0
+
+while chocolates_available and milk_cups and milkshakes_prepared < 5:
+
+    if chocolates_available[-1] <= 0 and milk_cups[0] <= 0:
+        chocolates_available.pop()
+        milk_cups.popleft()
+        continue
+    if chocolates_available[-1] <= 0:
+        chocolates_available.pop()
+        continue
+    if milk_cups[0] <= 0:
+        milk_cups.popleft()
+        continue
+
+    if chocolates_available[-1] == milk_cups[0]:
+        chocolates_available.pop()
+        milk_cups.popleft()
+        milkshakes_prepared += 1
+    else:
+        milk_cups.rotate(-1)  # equals to move the element at the last position
+        chocolates_available[-1] -= 5
+
+if milkshakes_prepared == 5:
+    print("Great! You made all the chocolate milkshakes needed!")
+else:
+    print("Not enough milkshakes.")
+
+print(f"Chocolate: {', '.join([str(x) for x in chocolates_available]) if chocolates_available else 'empty'}")
+print(f"Milk: {', '.join([str(x) for x in milk_cups]) if milk_cups else 'empty'}")
+
+# OR --------------------------------------------------------------------------------------------
+
 chocolates = deque(map(int, input().split(', ')))
 milk = deque(map(int, input().split(', ')))
 milkshakes = 0
