0a76ac8bcd1d5c804d0f52e5155f2e8fe81666d1
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-23 13:07:40 +0300

    add

--- matrix_exercices_1/matrix_of_palindromes.py
+++ matrix_exercices_1/matrix_of_palindromes.py
@@ -1,1 +1,7 @@
 rows, columns = [int(x) for x in input().split()]
+start_letter = ord('a')
+
+for row in range(rows):
+    for column in range(columns):
+        print(f"{chr(start_letter + row)}{chr(start_letter + row + column)}{chr(start_letter + row)}", end=" ")
+    print()
