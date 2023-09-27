1c69c4d6fd9a8cc284071e0d2d3466ea2ec5692e
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-27 21:07:37 +0300

    add

--- functions_advanced/cheese_showcase.py
+++ functions_advanced/cheese_showcase.py
@@ -1,8 +1,20 @@
 def sorting_cheeses(**kwargs):
-    for key, value in kwargs.items():
-        print(key)
-        [print(curr) for curr in sorted(value, reverse=True)]
-    return kwargs
+    sorted_dict = sorted(kwargs.items(), key=lambda kvp: (-len(kvp[1]), kvp[0]))
+    result = ""
+
+    for name, quantities in sorted_dict:
+        result += f"{name}\n"  # prints the name then adds new line
+        sorted_quantities = sorted(quantities, reverse=True)  # reverse the list in descending order
+        result += "\n".join([str(el) for el in sorted_quantities])  # prints each quantity and adds new line after each
+        result += "\n"  # prints new line after the last quantity before moving to the other name
+
+    return result
+
+    # We sort by length of the list -> len(kvp)
+    # We use [1] to sort by value and not by key -> kvp[1]
+    # We add minus because it is a descending order -> -len(kvp[1])
+    # We add parentheses because we should sort differently if there are two items with the same length
+    # It is called sort by, then by -> (-len(kvp[1]), kvp[0]))
 
 
-sorting_cheeses(Parmesan=[102, 120, 135], Camembert=[100, 100, 105, 500, 430], Mozzarella=[50, 125],)
+print(sorting_cheeses(Parmesan=[102, 120, 135], Camembert=[100, 100, 105, 500, 430], Mozzarella=[50, 125],))
--- functions_advanced/person_info.py
+++ functions_advanced/person_info.py
@@ -25,12 +25,12 @@ print(sorted(a))  # [-2, 3, 4, 10]
 # Using lambda to sort by key element:
 my_dict = {'Peter': 21, 'George': 18, 'John': 45}
 sorted_dict = sorted(my_dict.items(),
-                     key=lambda x: x[1])
+                     key=lambda x: x[0])  # if we sort by key we use '0'
 print(sorted_dict)  # [('George', 18), ('Peter', 21), ('John', 45)]
 
 my_dict = {'Peter': 21, 'George': 18, 'John': 45}
 sorted_dict = sorted(my_dict.items(),
-                     key=lambda kvp: kvp[1])
+                     key=lambda kvp: kvp[1]) # if we sort by value we use '1'
 for kvp in sorted_dict:
     print(kvp)
 # ('George', 18)
@@ -46,7 +46,6 @@ for kvp in sorted_dict:
 # ('John', 45)
 # ('Peter', 21)
 # ('George', 18)
-
 
 # Using reverse to sort dictionary by key in descending order
 my_dict = {'Peter': 21, 'George': 18, 'John': 45}
