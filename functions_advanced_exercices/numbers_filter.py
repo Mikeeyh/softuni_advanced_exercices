4b9179ad60215bffbd992d8f26a2850068afb142
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-29 01:22:20 +0300

    add

--- functions_advanced_exercices/numbers_filter.py
+++ functions_advanced_exercices/numbers_filter.py
@@ -1,4 +1,20 @@
 def even_odd_filter(**kwargs):
+    for key, value in kwargs.items():
+        if key == "even":
+            kwargs[key] = [num for num in value if num % 2 == 0]
+        elif key == "odd":
+            kwargs[key] = [num for num in value if num % 2 != 0]
+
+
+    return dict(sorted())
+
+
+print(even_odd_filter(odd=[1, 2, 3, 4, 10, 5], even=[3, 4, 5, 7, 10, 2, 5, 5, 2],))
+
+# OR ------------------------------------------------------------------------------------------------
+
+
+def even_odd_filter(**kwargs):
     result = {}
     for key, value in kwargs.items():
         if key == "even":
