7b27e30a95a2ecb5a8ff55bf412c45cd18ecf781
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-09-28 17:13:37 +0300

    Update age_assignment.py

--- functions_advanced_exercices/age_assignment.py
+++ functions_advanced_exercices/age_assignment.py
@@ -19,3 +19,20 @@ def age_assignment(*args, **kwargs):
 
 print(age_assignment('gosho', 'mike', 'paraya', g=20, m=10, p=15))
 
+# OR -----------------------------------------------------------------------
+
+
+def age_assignment(*names, **ages):
+    result = []
+    for name in names:
+        first_letter = name[0]
+        if first_letter in ages:
+            age = ages[first_letter]
+            result.append(f"{name} is {age} years old.")
+    result.sort()
+    return "\n".join(result)
+
+
+# Example usage:
+print(age_assignment("Alice", "Bob", "Charlie", A=30, B=25, C=35))
+
