f9b111f3c0e2da280b4ece8604330d83c1a13706
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2023-10-02 23:26:42 +0300

    Update math_operations.py

--- functions_advanced_exercices/math_operations.py
+++ functions_advanced_exercices/math_operations.py
@@ -1,4 +1,30 @@
 def math_operations(*args, **kwargs):
+    for i in range(len(args)):
+        if i % 4 == 0:
+            kwargs['a'] += args[i]
+        elif i % 4 == 1:
+            kwargs['s'] -= args[i]
+        elif i % 4 == 2:
+            if not args[i] == 0:
+                kwargs['d'] /= args[i]
+        elif i % 4 == 3:
+            kwargs['m'] *= args[i]
+
+    result = sorted(kwargs.items(), key=lambda x: (-x[1], x[0]))
+    final_result = []
+
+    for key, value in result:
+        final_result.append(f"{key}:{value:.1f}")
+
+    return '\n'.join(final_result)
+
+
+print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7,d=33, m=15))
+
+# OR ----------------------------------------------------------------------------
+
+
+def math_operations(*args, **kwargs):
     result_dict = {}
     operations = {"a": "add", "s": "subtract", "d": "divide", "m": "multiply"}
 
